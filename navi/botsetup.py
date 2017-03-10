cc31531ae93b047b447d49ba79351a79addcc109
Valerie Schmidt <valerie.schmidt@norimaconsulting.com>
2017-03-10 12:02:37 -0600

    Create navi/ dir for separate bot devel

---
+++ navi/botsetup.py
@@ -0,0 +1,15 @@
+#env/bin/python
+
+import os
+from slackclient import SlackClient
+import pdb
+
+BOT_NAME = 'navi'
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+if __name__ == '__main__':
+    api_call = slack_client.api_call("users.list")
+    if api_call.get('ok'):
+        users = api_call.get('members')
+        for user in users:
+
